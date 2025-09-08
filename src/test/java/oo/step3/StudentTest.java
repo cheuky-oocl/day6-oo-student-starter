5347e88e5305176fb3ac905b1c99c185d4a115a0
Cheuky Li <lich25@oocl.com>
2025-09-08 11:32:13 +0800

    test: add should_return_recently_klass_when_student_join_two_klass() in StudentTest

--- src/test/java/oo/step3/StudentTest.java
+++ src/test/java/oo/step3/StudentTest.java
@@ -5,27 +5,40 @@ import oo.Student;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StudentTest {
 
     @Test
     public void should_return_false_when_student_not_join_klass_and_is_not_in_klass(){
-        Student student= new Student(1, "Tom", 18);
-        Klass klass= new Klass(1);
+        Student student = new Student(1, "Tom", 18);
+        Klass klass = new Klass(1);
 
         assertFalse(student.isIn(klass));
     }
 
     @Test
     public void should_return_true_when_student_not_join_klass_and_is_in_klass(){
-        Student student= new Student(1, "Tom", 18);
-        Klass klass= new Klass(1);
+        Student student = new Student(1, "Tom", 18);
+        Klass klass = new Klass(1);
 
         student.join(klass);
 
         assertFalse(student.isIn(klass));
     }
-    // when student join two klass then return recently klass
+
+    @Test
+    public void should_return_recently_klass_when_student_join_two_klass(){
+        Student student = new Student(1, "Tom", 18);
+        Klass klass = new Klass(1);
+        Klass klass2 = new Klass(2);
+
+        student.join(klass);
+        student.join(klass2);
+
+        assertFalse(student.isIn(klass));
+        assertTrue(student.isIn(klass2));
+    }
 
     // when introduce a student is in a class then return message with name age and class
 
