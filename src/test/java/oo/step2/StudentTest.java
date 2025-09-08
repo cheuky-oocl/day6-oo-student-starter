c74d14786f6409024a78e7df11b01111a4b08769
Cheuky Li <lich25@oocl.com>
2025-09-08 10:46:09 +0800

    test: add test case StudentTest

--- src/test/java/oo/step2/StudentTest.java
+++ src/test/java/oo/step2/StudentTest.java
@@ -1,10 +1,33 @@
 package oo.step2;
 
+import oo.Person;
+import oo.Student;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StudentTest {
 
-    // when introduce a student, then return a message with name and age
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_a_person_of_student(){
+        Student student = new Student(1, "Tom", 18);
 
-    // when equals two same id student then return true
+        assertEquals("My name is Tom. I am 18 years old. I am a student", student.introduce());
+    }
 
-    // when equals two different id student then return true
+    @Test
+    public void should_return_true_when_equals_two_same_id_student(){
+        Student student = new Student(1, "Tom", 18);
+        Student student2 = new Student(1, "Tom", 18);
+
+        assertTrue(student.equals(student2));
+    }
+
+    @Test
+    public void should_return_false_when_equals_two_different_id_student(){
+        Student student = new Student(1, "Tom", 18);
+        Student student2 = new Student(2, "Michael", 30);
+
+        assertFalse(student.equals(student2));
+    }
 }
